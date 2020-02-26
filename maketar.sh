dc5f1ec11773b62450462f26dd30e96b02f097e5
Steve Kieu <steve.kieu@xvt.com.au>
2020-02-26 21:42:59 +1000

    Update build script

--- maketar.sh
+++ maketar.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-go build --tags "icu json1 fts5 secure_delete" -ldflags='-s -w' -o ~/Public/gnote-linux-amd64
+#go build --tags "icu json1 fts5 secure_delete" -ldflags='-s -w' -o ~/Public/gnote-linux-amd64
+go build --tags "json1 fts5 secure_delete" -ldflags='-s -w' -o ~/Public/gnote-linux-amd64
 cd ~/Public
 echo "Quit current gnote to allow update. Build windows version and manually sync to Public. Then hit enter"
 read _junk
 cp gnote-linux-amd64 ~/gnote/gnote
-for f in gnote-windows-amd64.7z gnote-bundle-windows-amd64.7z gnote-linux-amd64; do
+for f in gnote-bundle-windows-amd64.7z gnote-linux-amd64; do
 	aws s3 cp $f s3://xvt-public-repo/pub/devops/ --profile xvt_aws
 done
