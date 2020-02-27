03d3c60eff2d96702879f067f044c23454c5f2c6
Steve Kieu <steve.kieu@xvt.com.au>
2020-02-27 10:17:55 +1000

    Build using docker to cross ubuntu dist build

--- maketar.sh
+++ maketar.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
 #go build --tags "icu json1 fts5 secure_delete" -ldflags='-s -w' -o ~/Public/gnote-linux-amd64
-go build --tags "json1 fts5 secure_delete" -ldflags='-s -w' -o ~/Public/gnote-linux-amd64
+
+ARCH=$(uname -m)
+
+docker run --rm -v $(pwd):/work --entrypoint /usr/local/go/bin/go --workdir /work golang-ubuntu1804-build:latest build --tags "json1 fts5 secure_delete" -ldflags='-s -w' -o gnote-ubuntu1804-${ARCH}
+
+mv gnote-ubuntu1804-${ARCH} ~/Public/
+
+#go build --tags "json1 fts5 secure_delete" -ldflags='-s -w' -o ~/Public/gnote-linux-amd64
+
 cd ~/Public
 echo "Quit current gnote to allow update. Build windows version and manually sync to Public. Then hit enter"
 read _junk
-cp gnote-linux-amd64 ~/gnote/gnote
-for f in gnote-bundle-windows-amd64.7z gnote-linux-amd64; do
+
+cp -a gnote-ubuntu1804-${ARCH} ~/gnote/gnote
+for f in gnote-bundle-windows-amd64.7z gnote-ubuntu1804-${ARCH}; do
 	aws s3 cp $f s3://xvt-public-repo/pub/devops/ --profile xvt_aws
 done
