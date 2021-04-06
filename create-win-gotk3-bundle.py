bf0f73453d4920bdf38d871274adac7bac25dcbd
Steve Kieu <msh.computing@gmail.com>
2021-04-06 21:35:28 +1000

    Update the dll list and create bundle

--- create-win-gotk3-bundle.py
+++ create-win-gotk3-bundle.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 # shell in windows yuk, when reading the dll_files.lst and convert into the cp
 # command it just add some $ char and thus file not found.
 # python shutil works and I dont bother hacking bash to work
@@ -19,6 +21,7 @@ mkdir bin lib share
 
 cp -a /mingw64/lib/gdk-pixbuf-2.0 lib/
 cp -a /usr/share/glib-2.0 share/
+cp -a /mingw64/share/glib-2.0/schemas/* share/glib-2.0/schemas/
 cp -a /mingw64/share/icons share/
 cp /c/ansible_install/gnote/gnote.exe bin/
 cp -a /c/ansible_install/gnote/glade bin/
@@ -28,14 +31,13 @@ cp -a /c/ansible_install/gnote/icons bin/
 data = open("/c/ansible_install/gnote/dll_files.lst", "r").read().splitlines()
 
 # Now copy dlls into the bin dir
-ptn = re.compile(r"^([^\-]+)\-[^\s]+$")
 
 for f in data:
-    m = ptn.search(f)
-    if m:
-        _f = m.group(1)
-        _f1 = glob("/mingw64/bin/%s*" %_f)[0]
-        shutil.copy(_f1, "/c/ansible_install/gnote-windows-bundle/bin/")
+    if not f.startswith('#'):
+        print(f"copying '{f}'")
+        #_f1 = glob(f"/mingw64/bin/{_f}*")[0]
+        f1 = f"/mingw64/bin/{f}"
+        shutil.copy(f1, "/c/ansible_install/gnote-windows-bundle/bin/")
 
 os.chdir("/c/ansible_install")
 archive_name = "gnote-windows-bundle-{VER}".format(VER=os.getenv("BUILD_VERSION", "v0.1"))
